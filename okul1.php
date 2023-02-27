6edb75ba226d7fc6f4fd9f2c3a7fbf349cde77da
Cesur Huseynzade <97361627+cesurh@users.noreply.github.com>
2023-02-27 19:42:40 +0300

    Update okul1.php

--- okul1.php
+++ okul1.php
@@ -1,19 +1,7 @@
 <?php
-    $sunucu_adi = "localhost";
-    $kullanici_adi = "root";
-    $sifre = "#sifre";
-    $veri_tabani = "kutuphane";
-    
-    $baglanti = new mysqli($sunucu_adi, $kullanici_adi, $sifre, $veri_tabani, 3306);
 
-    if($baglanti->connect_error)
-        die("Bağlantı sağlanamadı:".$baglanti->connect_error);
-    /*else
-      echo "Bağlantı başarılı";*/
-
-
+include("baglanti.php")
 ?>
-
 
 <?php
 $kategoris_err="";
